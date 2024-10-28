7fc0f915ba71d02cee688804799b540cfd8f15e0
Kaano1 <ahmetyumusakdiken13@gmail.com>
2024-10-28 11:36:40 +0300

    Develop: touch screen added

--- mouse_click.py
+++ mouse_click.py
@@ -1,13 +1,40 @@
 import pyautogui
 import time
 
-pyautogui.moveTo(196, 33) # you can set the position of the mouse (x, y)
-while (True):
-	time.sleep(29)
-	if (pyautogui.position() != (196, 33)):
-		break
-	time.sleep(1)
-	pyautogui.click() # click the mouse
+next_x = 1878
+next_y = 960
 
+screen_start_y = 290
+screen_end_y = 810 
+
+screen_start_x = 977
+screen_end_x = 1904
+
+start_time = time.time()  # start time
+pyautogui.moveTo(next_x, next_y) # you can set the position of the mouse (x, y)
+
+import pyautogui
+import time
+
+def watchTime(start_time):
+    now_time = time.time() # current time
+    elapsed_time = now_time - start_time # elapsed time
+    if elapsed_time >= 60: # 60 seconds
+        y = screen_start_y
+        while y <= screen_end_y:
+            x = screen_start_x
+            while x <= screen_end_x:
+                pyautogui.moveTo(x, y)
+                pyautogui.click()
+                x += 50
+            y += 40
+        start_time = time.time()
+    return start_time
+
+while (True):
+	pyautogui.click() # click the mouse
+	time.sleep(5)
+	pyautogui.moveTo(next_x, next_y) # you can set the position of the mouse (x, y)
+	start_time = watchTime(start_time)
 
 print("Done")
